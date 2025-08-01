6f0465a04761a4f26af13cb6e53cc4721b2b28ef
Carddera1842 <carddera1842@gmail.com>
2025-08-01 16:50:01 -0500

    First Commit

---
+++ jobportal/src/main/java/com/pluralsight/jobportal/model/Application.java
@@ -0,0 +1,20 @@
+package com.pluralsight.jobportal.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "applications")
+public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "job_id", nullable = false)
+    private Job job;
+}
